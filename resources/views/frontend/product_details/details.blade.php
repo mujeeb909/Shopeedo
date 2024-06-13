8e4eb7b3fa985e64ba3d5a4b207bf27d6aab6cd6
Sabtain Ali <sabtain.ali@logiqon.co>
2024-06-13 14:05:22 +0500

    compare icons update in product page 6/13/24

--- resources/views/frontend/product_details/details.blade.php
+++ resources/views/frontend/product_details/details.blade.php
@@ -73,7 +73,12 @@
                     <!-- Add to compare button -->
                     <a href="javascript:void(0)" onclick="addToCompare({{ $detailedProduct->id }})"
                         class="fs-14 text-dark opacity-60 has-transitiuon hov-opacity-100">
-                        <i class="las la-sync mr-1"></i>
+                        {{-- <i class="las la-sync mr-1"></i> --}}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16.668" height="13" viewBox="0 0 16 16">
+                            <path id="_9f8e765afedd47ec9e49cea83c37dfea" data-name="9f8e765afedd47ec9e49cea83c37dfea"
+                                d="M18.037,5.547v.8a.8.8,0,0,1-.8.8H7.221a.4.4,0,0,0-.4.4V9.216a.642.642,0,0,1-1.1.454L2.456,6.4a.643.643,0,0,1,0-.909L5.723,2.227a.642.642,0,0,1,1.1.454V4.342a.4.4,0,0,0,.4.4H17.234a.8.8,0,0,1,.8.8Zm-3.685,4.86a.642.642,0,0,0-1.1.454v1.661a.4.4,0,0,1-.4.4H2.84a.8.8,0,0,0-.8.8v.8a.8.8,0,0,0,.8.8H12.854a.4.4,0,0,1,.4.4V17.4a.642.642,0,0,0,1.1.454l3.267-3.268a.643.643,0,0,0,0-.909Z"
+                                transform="translate(-2.037 -2.038)" fill="#ACACB1" />
+                        </svg>
                         {{ translate('Add to Compare') }}
                     </a>
                 </div>
